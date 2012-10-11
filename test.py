8c412317f28b817a10ae9fd18a5a258ffc2d29c7
Kenya Stewart <kenyavs@kenyavs-Inspiron-N5040.(none)>
2012-10-11 12:51:45 -0400

    testing things out

---
+++ test.py
@@ -0,0 +1,8 @@
+def notify(s):
+    print(s)
+
+d = {"notify": notify}
+
+d["notify"]("Hello, world! Yay! Again!")
+
+URL_routing = {'/create': 'new_post', '/display_post_links': 'display_post_links'}
